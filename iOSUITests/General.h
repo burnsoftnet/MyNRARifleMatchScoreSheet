236e22063f048c7cf8a16cb521c07b893c46cdf1
Joe M <joe.mireles@burnsoft.net>
2020-12-24 22:05:28 -0500

    added comments

--- iOSUITests/General.h
+++ iOSUITests/General.h
@@ -12,6 +12,12 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface General: NSObject
+/*!
+ @discussion Disect string to software based keyboard
+ @brief  pass a string to be taken apart character by character to send to the software based keyboard.
+ @param app - iOS app instance
+ @param value - the string that you want to take apart to send to keyboard
+ */
 +(void)sendTextToKeyBoard:(XCUIApplication *) app :(NSString *) value;
 @end
 
--- iOSUITests/General.m
+++ iOSUITests/General.m
@@ -10,6 +10,13 @@
 
 @implementation General: NSObject
 
+#pragma mark
+/*!
+ @discussion Disect string to software based keyboard
+ @brief  pass a string to be taken apart character by character to send to the software based keyboard.
+ @param app - iOS app instance
+ @param value - the string that you want to take apart to send to keyboard
+ */
 +(void)sendTextToKeyBoard:(XCUIApplication *) app :(NSString *) value
 {
     //Start code section for string diesect and send
